0ce537e5af3943c3aa9b83f50b4dac1e2a1b8a85
old.man <sunth0101@126.com>
2018-05-08 22:58:06 +0800

    文件列表界面微调整。。。

---
+++ src/QnStorageClient/QnStorageClient/Converters/EmptyToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace QnStorageClient.Converters
+{
+    public class EmptyToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null || string.IsNullOrEmpty(value.ToString()))
+            {
+                return Visibility.Collapsed;
+            }
+
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
