5421e710d9c17e65b65416f900a788b9ade80575
old.man <sunth0101@126.com>
2018-06-19 23:09:49 +0800

    add unhandle exception helper

--- src/QnStorageClient/QnStorageClient/App.xaml.cs
+++ src/QnStorageClient/QnStorageClient/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
@@ -34,6 +35,16 @@ namespace QnStorageClient
         {
             InitializeComponent();
             Suspending += OnSuspending;
+
+            ExceptionHandlingSynchronizationContext
+                .Register()
+                .UnhandledException += App_UnhandledException; ;
+        }
+
+        private void App_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+            NotificationService.ShowMessage(e.Exception?.Message,2000);
         }
 
         /// <summary>
@@ -103,4 +114,146 @@ namespace QnStorageClient
             deferral.Complete();
         }
     }
+
+    internal class ExceptionHandlingSynchronizationContext : SynchronizationContext
+    {
+        /// <summary>
+        /// Registration method.  Call this from OnLaunched and OnActivated inside the App.xaml.cs
+        /// </summary>
+        /// <returns></returns>
+        public static ExceptionHandlingSynchronizationContext Register()
+        {
+            var syncContext = Current;
+            if (syncContext == null)
+                throw new InvalidOperationException("Ensure a synchronization context exists before calling this method.");
+
+
+            var customSynchronizationContext = syncContext as ExceptionHandlingSynchronizationContext;
+
+
+            if (customSynchronizationContext == null)
+            {
+                customSynchronizationContext = new ExceptionHandlingSynchronizationContext(syncContext);
+                SetSynchronizationContext(customSynchronizationContext);
+            }
+
+
+            return customSynchronizationContext;
+        }
+
+        /// <summary>
+        /// Links the synchronization context to the specified frame
+        /// and ensures that it is still in use after each navigation event
+        /// </summary>
+        /// <param name="rootFrame"></param>
+        /// <returns></returns>
+        public static ExceptionHandlingSynchronizationContext RegisterForFrame(Frame rootFrame)
+        {
+            if (rootFrame == null)
+                throw new ArgumentNullException("rootFrame");
+
+            var synchronizationContext = Register();
+
+            rootFrame.Navigating += (sender, args) => EnsureContext(synchronizationContext);
+            rootFrame.Loaded += (sender, args) => EnsureContext(synchronizationContext);
+
+            return synchronizationContext;
+        }
+
+        private static void EnsureContext(SynchronizationContext context)
+        {
+            if (Current != context)
+                SetSynchronizationContext(context);
+        }
+
+
+        private readonly SynchronizationContext _syncContext;
+
+
+        public ExceptionHandlingSynchronizationContext(SynchronizationContext syncContext)
+        {
+            _syncContext = syncContext;
+        }
+
+
+        public override SynchronizationContext CreateCopy()
+        {
+            return new ExceptionHandlingSynchronizationContext(_syncContext.CreateCopy());
+        }
+
+
+        public override void OperationCompleted()
+        {
+            _syncContext.OperationCompleted();
+        }
+
+
+        public override void OperationStarted()
+        {
+            _syncContext.OperationStarted();
+        }
+
+
+        public override void Post(SendOrPostCallback d, object state)
+        {
+            _syncContext.Post(WrapCallback(d), state);
+        }
+
+
+        public override void Send(SendOrPostCallback d, object state)
+        {
+            _syncContext.Send(d, state);
+        }
+
+
+        private SendOrPostCallback WrapCallback(SendOrPostCallback sendOrPostCallback)
+        {
+            return state =>
+            {
+                try
+                {
+                    sendOrPostCallback(state);
+                }
+                catch (Exception ex)
+                {
+                    if (!HandleException(ex))
+                        throw;
+                }
+            };
+        }
+
+        private bool HandleException(Exception exception)
+        {
+            if (UnhandledException == null)
+                return false;
+
+            var exWrapper = new UnhandledExceptionEventArgs
+            {
+                Exception = exception
+            };
+
+            UnhandledException(this, exWrapper);
+
+#if DEBUG && !DISABLE_XAML_GENERATED_BREAK_ON_UNHANDLED_EXCEPTION
+            if (System.Diagnostics.Debugger.IsAttached) System.Diagnostics.Debugger.Break();
+#endif
+
+            return exWrapper.Handled;
+        }
+
+
+        /// <summary>
+        /// Listen to this event to catch any unhandled exceptions and allow for handling them
+        /// so they don't crash your application
+        /// </summary>
+        public event EventHandler<UnhandledExceptionEventArgs> UnhandledException;
+    }
+
+    public class UnhandledExceptionEventArgs : EventArgs
+    {
+        public bool Handled { get; set; }
+        public Exception Exception { get; set; }
+    }
+
+
 }
